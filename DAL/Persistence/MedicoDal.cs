e284da2034d3e4aaadac848396c86821c65f1d57
andreivercosa <andreivercosa@gmail.com>
2019-01-30 21:53:13 -0400

    sistemaclinica_05

---
+++ DAL/Persistence/MedicoDal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using BLL.Model;
+using System.Collections.Generic;
+
+namespace DAL.Persistence
+{
+    public class MedicoDal : Conexao
+    {
+        public void Salvar(Medico medico)
+        {
+            try
+            {
+                AbrirConexao();
+                var sql = "INSERT INTO medico(idEspecialidade, nome, crm, dtCadastro)" +
+                          "VALUES(@idEspecialidade, @nome, @crm, CURRENT_TIMESTAMP())";
+
+                command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@idEspecialidade", medico.IdEspecialidade);
+                command.Parameters.AddWithValue("@nome", medico.Nome);
+                command.Parameters.AddWithValue("@crm", medico.CRM);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao registrar dado " + erro.Message + erro.ToString());
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        public List<Medico> Listar()
+        {
+            try
+            {
+                AbrirConexao();
+                var sql = "SELECT * FROM medico";
+                command = new MySqlCommand(sql, connection);
+                dataReader = command.ExecuteReader();
+
+                List<Medico> listaMedico = new List<Medico>();
+
+                while (dataReader.Read())
+                {
+                    Medico medico = new Medico();
+
+                    medico.Id = Convert.ToInt32(dataReader["id"]);
+                    medico.Nome = dataReader["nome"].ToString();
+                    medico.Nome = dataReader["crm"].ToString();
+                    medico.DtCadastro = dataReader["dtCadastro"].ToString();
+
+                }
+                return listaMedico;
+
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro ao registrar dado " + erro.Message + erro.ToString());
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        public MedicoDal()
+        {
+        }
+    }
+}
---
+++ View/Pages/MedicoCadastro.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using BLL.Model;
+using DAL.Persistence;
+using System.Threading;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+namespace View.Pages
+{
+
+    public partial class MedicoCadastro : System.Web.UI.Page
+    {
+        public void Page_Load(object sender, EventArgs e)
+        {
+            bindEspecialidade();
+        }
+
+        public void btnCadastrarMedico(object sender, EventArgs e)
+        {
+            try
+            {
+                Medico medico = new Medico();
+                medico.IdEspecialidade = Int32.Parse(idEspecialidade.SelectedValue);
+                medico.Nome = nome.Text;
+                medico.CRM = crm.Text;
+
+
+                MedicoDal medicoDal = new MedicoDal();
+                medicoDal.Salvar(medico);
+
+                nome.Text = "";
+                crm.Text = "";
+
+
+                string msg = "Medico " + medico.Nome + " foi cadastrado com Sucesso";
+                lblMensagem.Attributes.CssStyle.Add("color", "green");
+                lblMensagem.Text = msg;
+
+
+            }
+            catch (Exception erro)
+            {
+                throw new Exception("Erro");
+            }
+        }
+
+        public void bindEspecialidade()
+        {
+            EspecialidadeDal especialidadeDal = new EspecialidadeDal();
+            List<Especialidade> listaEspecialidade = new List<Especialidade>();
+
+            listaEspecialidade = especialidadeDal.Listar();
+
+            foreach (var especialidade in listaEspecialidade)
+            {
+                idEspecialidade.Items.Insert(0, new ListItem(especialidade.Descricao, especialidade.Id.ToString()));
+            }
+
+
+        }
+    }
+}
